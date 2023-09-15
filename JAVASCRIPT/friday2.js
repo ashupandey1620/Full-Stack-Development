028f84ac378d89373cb0b7c7c962a03bca588c2e
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-15 14:59:57 +0530

    modified:   JAVASCRIPT/friday2.js

--- JAVASCRIPT/friday2.js
+++ JAVASCRIPT/friday2.js
@@ -11,3 +11,9 @@ for(let i=0;i<100;i++)
 an.party();
 
 //objects are created used and discarded
+
+//We can store each distinct object in its own variable.
+
+//We call this having multiple instances of the same class 
+
+//Each instances has its own copy of the class
