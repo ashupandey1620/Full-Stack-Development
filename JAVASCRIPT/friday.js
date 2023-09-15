692b17b4075fc36a213b826fd17f6c0ff7690898
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-15 14:08:15 +0530

    modified:   JAVASCRIPT/friday.js

--- JAVASCRIPT/friday.js
+++ JAVASCRIPT/friday.js
@@ -1,7 +1,7 @@
-var isEven = (element)=>{
-    element%2 == 0;
-}
-console.log(isEven(5));
+// var isEven = (element)=>{
+//     element%2 == 0;
+// }
+// console.log(isEven(5));
 //till the time it is true, It will carry on but as soon as it encounters false then it will assign resukt as true
-var result =[2,3,6,8].every(isEven);
+var result =[2,3,6,8].every((e)=>(e%2==0));
 console.log(result);
