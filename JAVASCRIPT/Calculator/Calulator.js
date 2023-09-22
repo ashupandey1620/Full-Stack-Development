a1ac9aab9059078b075ddb608f186074c455cec2
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-22 14:03:28 +0530

    modified:   JAVASCRIPT/Calculator/Calulator.js

--- JAVASCRIPT/Calculator/Calulator.js
+++ JAVASCRIPT/Calculator/Calulator.js
@@ -1,7 +1,58 @@
-class Calculator{
-    constructor(num1,num2){
-        this.num1 = num1;
-        this.num2 = num2;
+
+
+function Calculator() {
+    this.result = 0;
+  }
+  
+  
+  Calculator.prototype.add = function (value) {
+    this.result += value;
+  };
+  
+
+  Calculator.prototype.subtract = function (value) {
+    this.result -= value;
+  };
+  
+
+  Calculator.prototype.multiply = function (value) {
+    this.result *= value;
+  };
+  
+
+  Calculator.prototype.divide = function (value) {
+    if (value !== 0) {
+      this.result /= value;
+    } else {
+      console.error("Cannot divide by zero.");
     }
-    
-}
+  };
+  
+
+  Calculator.prototype.getResult = function () {
+    return this.result;
+  };
+  
+
+  Calculator.prototype.clear = function () {
+    this.result = 0;
+  };
+  
+
+  const calculator = new Calculator();
+  
+  calculator.add(10);
+  console.log("Result after adding 10:", calculator.getResult());
+  
+  calculator.subtract(5);
+  console.log("Result after subtracting 5:", calculator.getResult());
+  
+  calculator.multiply(2);
+  console.log("Result after multiplying by 2:", calculator.getResult());
+  
+  calculator.divide(4);
+  console.log("Result after dividing by 4:", calculator.getResult());
+  
+  calculator.clear();
+  console.log("Result after clearing:", calculator.getResult());
+  
